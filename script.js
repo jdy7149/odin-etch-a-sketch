8031ed9b8e42e03c2abf9203e97f5d82fced757e
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-05-31 01:48:47 +0900

    Add input range to modify grid size

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-const container = document.querySelector('.container');
-console.log(container);
-for (let i = 0; i < 16; i++){
+const grid = document.querySelector('.grid');
+const gridRange = document.querySelector('#grid-range');
+const gridSpan = document.querySelector('#grid-size');
+
+gridSize = gridRange.value;
+gridSpan.textContent = gridSize;
+
+// Initailize grid
+function generateGrid(side){
+    grid.remo
+}
+for (let i = 0; i < gridSize; i++){
     const row = document.createElement('div');
     row.classList.add('row');
-    for (let j = 0; j < 16; j++){
+
+    for (let j = 0; j < gridSize; j++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
         row.appendChild(cell);
     }
-    container.appendChild(row);
-}
-
-function random(number){
-    return Math.floor(Math.random() * number);
+    grid.appendChild(row);
 }
 
 const cells = document.querySelectorAll('.cell');
 
-function changeHoveringColor(color, random = null){
-    cells.forEach(elem => elem.addEventListener('mouseover', evt => {
-        evt.target.style.backgroundColor = random === null ? color : `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-    }));
-}
-
+// Initialize cells' eventhandler
 cells.forEach(elem => elem.addEventListener('mouseover', evt => {
     evt.target.style.backgroundColor = 'black';
 }));
+
+function random(number){
+    return Math.floor(Math.random() * number);
+}
 
 const radios = document.querySelectorAll('input[type="radio"]');
 
 radios.forEach(elem => elem.addEventListener('change', evt => {
     const id = evt.target.getAttribute('id');
     
-    if (id === 'random'){
-        changeHoveringColor('', random);
-    } else if (id === 'eraser'){
-        changeHoveringColor('white');
-    } else if (id === 'black'){
-        changeHoveringColor('black');
+    let hoverHandler = null;
+
+    switch (id){
+        case 'random':
+            hoverHandler = evt => evt.target.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+            break;
+        case 'black':
+            hoverHandler = evt => evt.target.style.backgroundColor = 'black';
+            break;
+        case 'eraser':
+            hoverHandler = evt => evt.target.style.backgroundColor = 'white';
+            break;
+        default:
+            hoverHandler = evt => evt.target.style.backgroundColor = 'black';
     }
 
+    cells.forEach(elem => elem.addEventListener('mouseover', hoverHandler));
 }));
